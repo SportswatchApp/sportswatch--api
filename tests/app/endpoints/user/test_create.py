48bcd546e2662780d7d59a6c9b2d3b8f8fd9ca94
tobiasdybdahl <tdybda17@student.aau.dk>
2020-11-01 15:51:38 +0100

    added /v1/ to api url

--- tests/app/endpoints/user/test_create.py
+++ tests/app/endpoints/user/test_create.py
@@ -13,11 +13,11 @@ class TestCreateUserEndpoint(TestCase):
         self.client = client.Client()
 
     def test_when_no_data_provided_Should_get_bad_request(self):
-        response = self.client.post('/api/users/')
+        response = self.client.post('/api/v1/users/')
         self.assertEqual(400, response.status_code)
 
     def test_when_user_already_exist_Should_get_already_reported(self):
         with mock.patch.object(Listener, 'get_response') as m:
             m.return_value = (403, '')
-            response = self.client.post('/api/users/')
+            response = self.client.post('/api/v1/users/')
             self.assertEqual(403, response.status_code)
