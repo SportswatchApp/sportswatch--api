e12b77cf8c56fcc8b7dec21b9e0b6837d50d5a3a
tobiasdybdahl <tdybda17@student.aau.dk>
2020-11-02 00:21:37 +0100

    small changes

--- app/usecases/create_user/listener.py
+++ app/usecases/create_user/listener.py
@@ -5,12 +5,12 @@ class Listener(listener.Listener, listener.SuccessListener):
 
     def handle_success(self, data=None):
         self.response = (
-            200, data
+            201, data
         )
 
     def handle_unique_username(self):
         self.response = {
-            'status': 403,
+            'status': 409,
             'da': 'E-mailen er allerede i brug',
             'en': 'E-mail already in use'
         }
