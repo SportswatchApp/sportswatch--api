912ff37b023cef1921700951eb695d333cb0feee
tobiasdybdahl <tdybda17@student.aau.dk>
2020-11-03 23:08:56 +0100

    prod changes

--- sportswatch/prod_settings.py
+++ sportswatch/prod_settings.py
@@ -8,9 +8,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = '__SECRET__'
+SECURE_SSL_REDIRECT = True
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['test.sportswatchapp.dk']
 
