d33f129f79f3d6630449f4abdfabfe29a8e7234b
tobiasdybdahl <tdybda17@student.aau.dk>
2020-11-19 22:36:35 +0100

    openapi-schema added with documentation

--- sportswatch/urls.py
+++ sportswatch/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path, include
+from sportswatch.schema import SchemaView
 
 urlpatterns = [
     path('api/v1/', include('app.urls')),
+    path('openapi/', SchemaView.as_view(), name='schema view'),
 ]
